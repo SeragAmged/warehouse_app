963c3ebf2895d021e6b6773627a1cf568fa6f17c
AnoPiS <omer.mkawy.om@gmail.com>
2023-12-20 09:46:15 +0200

    checkout dropbutton

--- lib/modules/item/item_screen.dart
+++ lib/modules/item/item_screen.dart
@@ -274,7 +274,7 @@ class _ItemScreenState extends State<ItemScreen> {
                                                               String>(
                                                             value: value,
                                                             child: SizedBox(
-                                                                width: 150,
+                                                                width: 130,
                                                                 child: Padding(
                                                                   padding:
                                                                       const EdgeInsets
